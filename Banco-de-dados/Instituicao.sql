ec0a2872f0dfa713e9e99d8a73d2e0c4a4b720ca
unknown <nicolas.g.ferreira9@aluno.senai.br>
2026-08-26 17:06:52 -0300

    sql

--- Banco-de-dados/Instituicao.sql
+++ Banco-de-dados/Instituicao.sql
@@ -80,3 +80,74 @@ CREATE TABLE evento_palestrante
 codigo_evento_FK INT, FOREIGN KEY (codigo_evento_FK) REFERENCES evento(codigo_evento),
 id_FK INT, FOREIGN KEY (id_FK) REFERENCES palestrante(id_palestrante)
 );
+
+INSERT INTO aluno (nome, numero_matricula, e_mail, curso, SITUACAO_fk)
+VALUES ('Henrique', 2026002, 'bili@aluno.senai.br', 'programador de sistemas', 'aprovado');
+
+INSERT INTO aluno (nome, numero_matricula, e_mail, curso, SITUACAO_fk)
+VALUES ('lauro', 2026001, 'lauro@gmail.com', 'programador de sistemas', 'aprovado');
+
+INSERT INTO aluno (nome, numero_matricula, e_mail, curso, SITUACAO_fk)
+VALUES ('Igor', 2026003, 'ingor@aluno.senai.br', 'programador de sistemas', 'reprovado');
+
+INSERT INTO aluno (nome, numero_matricula, e_mail, curso, SITUACAO_fk)
+VALUES ('Cleiton', 2026004, 'cleiton@gmail.com', 'programador de sistemas', 'aprovado');
+
+INSERT INTO aluno (nome, numero_matricula, e_mail, curso, SITUACAO_fk)
+VALUES ('Cleverson', 2026005, 'clevson@gmail.com', 'programador de sistemas', 'aprovado');
+
+INSERT INTO aluno (nome, numero_matricula, e_mail, curso, SITUACAO_fk)
+VALUES ('Octavio', 2026006, 'octivio@gmail.com', 'programador de sistemas', 'aprovado');
+
+INSERT INTO aluno (nome, numero_matricula, e_mail, curso, SITUACAO_fk)
+VALUES ('douglas', 2026007, 'douglos@gmail.com', 'programador de sistemas', 'aprovado');
+
+INSERT INTO aluno (nome, numero_matricula, e_mail, curso, SITUACAO_fk)
+VALUES ('yasmin', 2026008, 'jasmini@gmail.com', 'programador de sistemas', 'aprovado');
+
+INSERT INTO aluno (nome, numero_matricula, e_mail, curso, SITUACAO_fk)
+VALUES ('ronaldo', 2026009, 'meirelhos@gmail.com', 'programador de sistemas', 'aprovado');
+
+INSERT INTO aluno (nome, numero_matricula, e_mail, curso, SITUACAO_fk)
+VALUES ('jonas', 20260010, 'joracildo@gmail.com', 'programador de sistemas', 'aprovado');
+
+INSERT INTO inscricao (situacao, data_do_vento)
+VALUES ('reprovado', '2026-08-24');
+
+UPDATE departamento
+SET area_atucao = 'alimentação'
+WHERE codigo_departamento = 2026100;
+
+table departamento
+
+INSERT INTO departamento (codigo_departamento, nome, area_atucao)
+VALUES (2026110, 'alimentos', 'limentação');
+
+INSERT INTO departamento (codigo_departamento, nome, area_atucao)
+VALUES (2026120, 'contrução', 'construir');
+
+INSERT INTO departamento (codigo_departamento, nome, area_atucao)
+VALUES (2026130, 'financeiro', 'dinheiro');
+
+INSERT INTO departamento (codigo_departamento, nome, area_atucao)
+VALUES (2026140, 'academico', 'cuidar dos alunos');
+
+INSERT INTO departamento (codigo_departamento, nome, area_atucao)
+VALUES (2026150, 'cotinas', 'corina');
+
+INSERT INTO departamento (codigo_departamento, nome, area_atucao)
+VALUES (2026160, 'prograação', 'programar');
+
+INSERT INTO departamento (codigo_departamento, nome, area_atucao)
+VALUES (2026170, 'gabriels', 'todos os gabriels');
+
+INSERT INTO departamento (codigo_departamento, nome, area_atucao)
+VALUES (2026180, 'teste', 'testes');
+
+INSERT INTO departamento (codigo_departamento, nome, area_atucao)
+VALUES (2026190, 'caimento', 'cair');
+
+INSERT INTO departamento (codigo_departamento, nome, area_atucao)
+VALUES (2026111, 'orações', 'orar');
+
+
---
+++ Banco-de-dados/exercicios sql.sql
@@ -0,0 +1,109 @@
+CREATE TABLE alunos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    idade INT,
+    cidade VARCHAR(50),
+    curso VARCHAR(50),
+    nota NUMERIC(4,2)
+);
+
+INSERT INTO alunos (nome, idade, cidade, curso, nota) VALUES
+('Ana Silva', 18, 'Curitiba', 'Programação', 8.5),
+('Bruno Santos', 21, 'Londrina', 'Redes', 7.0),
+('Carlos Souza', 19, 'Curitiba', 'Programação', 9.0),
+('Daniel Oliveira', 25, 'Maringá', 'Banco de Dados', 6.5),
+('Eduarda Lima', 20, 'Curitiba', 'Redes', 8.0),
+('Felipe Costa', 17, 'Londrina', 'Programação', 5.5),
+('Gabriela Alves', 22, 'Maringá', 'Banco de Dados', 9.5),
+('Henrique Rocha', 19, 'Curitiba', 'Redes', 7.5),
+('Isabela Martins', 20, 'Londrina', 'Programação', 8.8),
+('João Pereira', 23, 'Curitiba', 'Banco de Dados', 6.0),
+('Karen Mendes', 18, 'Maringá', 'Programação', 9.2),
+('Lucas Ferreira', 26, 'Curitiba', 'Redes', 7.8);
+
+SELECT * FROM alunos;
+
+SELECT nome, idade, curso FROM alunos;
+
+SELECT nome, cidade FROM alunos;
+
+SELECT nome, idade +1 AS idade_futura
+FROM alunos;
+
+SELECT * FROM alunos 
+WHERE idade >= 18;
+
+SELECT * FROM alunos 
+WHERE cidade ='Curitiba';
+
+SELECT * FROM alunos 
+WHERE nota > 8;
+
+SELECT * FROM alunos 
+WHERE NOT cidade ='Curitiba';
+
+SELECT * FROM alunos 
+WHERE curso ='Programação'
+AND idade >= 18;
+
+SELECT * FROM alunos 
+WHERE cidade ='Curitiba'
+OR cidade = 'Londrina';
+
+SELECT * FROM alunos 
+WHERE cidade = 'Curitiba'
+AND nota >= 8;
+
+SELECT * FROM alunos 
+WHERE idade BETWEEN 18 AND 20;
+
+SELECT * FROM alunos 
+WHERE nota BETWEEN 7 AND 9;
+
+SELECT * FROM alunos 
+WHERE curso IN ('Programação', 'Redes');
+
+SELECT * FROM alunos 
+WHERE cidade IN ('Curitiba', 'Londrina', 'Maringá');
+
+
+SELECT * FROM alunos 
+WHERE nome LIKE 'A%';
+
+SELECT * FROM alunos 
+WHERE nome LIKE '%a';
+
+SELECT * FROM alunos 
+WHERE nome LIKE '%ana%';
+
+SELECT * FROM alunos 
+WHERE nome ILIKE 'c%';
+
+SELECT DISTINCT cidade
+FROM alunos;
+
+SELECT DISTINCT curso
+FROM alunos;
+
+SELECT * FROM alunos 
+ORDER BY nome ASC;
+
+SELECT * FROM alunos 
+ORDER BY idade ASC;
+
+SELECT * FROM alunos 
+ORDER BY idade DESC;
+
+SELECT * FROM alunos 
+ORDER BY cidade ASC, nome ASC;
+
+SELECT * FROM alunos
+LIMIT 5;
+
+SELECT * FROM alunos
+ORDER BY nota DESC
+LIMIT 5;
+
+SELECT * FROM alunos
+LIMIT 3
+OFFSET 3;
